3e91cfdec42beccb2bd7abaacc6bb09ae2581db8
ispan <ispan@DESKTOP-PAKSETB>
2022-11-03 11:04:54 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("請輸入時間:");
+			string input = Console.ReadLine();
+			DateTime intputday = Convert.ToDateTime(input);
+			Console.WriteLine(intputday);
+			switch (intputday.DayOfWeek)
+			{
+				case DayOfWeek.Sunday:
+				case DayOfWeek.Saturday:
+					Console.WriteLine("今天是禮拜六,不是交易日");
+					break;
+				default:
+
+					if (intputday.Hour < 9)
+					{
+						Console.WriteLine("現在不是交易時間");
+					}
+					else if (intputday.Hour > 13)
+					{
+						Console.WriteLine("現在不是交易時間");
+					}
+					else if (intputday.Hour >= 13 && intputday.Minute >= 30)
+					{
+						Console.WriteLine("現在不是交易時間");
+					}
+					else { Console.WriteLine("現在是交易時間"); }
+
+					break;
+			}
+		}
+	}
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace TestProject1
+{
+	public class Tests
+	{
+		//我程式是寫出來了,但測試要如何寫沒有頭緒
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
